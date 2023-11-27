80f1c48ecd5517eeb91bbe1891292caa46849ae1
eshad <fidaeshad10@gmail.com>
2023-11-27 06:29:42 -0600

    first commit

---
+++ example/interfaces_multiple_interface_implementation_example.dart
@@ -0,0 +1,6 @@
+import 'package:interfaces_multiple_interface_implementation/interfaces_multiple_interface_implementation.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
